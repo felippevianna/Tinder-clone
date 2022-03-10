08e65ae8a7d91867cb29a94a24ff574896a50398
Felippe Vianna <felippe.conduru@gmail.com>
2022-03-10 20:45:05 -0300

    beginning of the auth modal

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,10 +1,53 @@
+import { useState } from 'react'
+
 const AuthModal = ({setShowModal}) => {
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [confirmPassword, setConfirmPassword] = useState(null)
+    const [error, setError] = useState(null)
+    
     const handleClick = () => {
         setShowModal(false)
     }
+
+    const isSignUp = true
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
-        <div>
-            <div onClick={handleClick}>X</div>
+        <div className="auth-modal">
+            <div className="close-icon" onClick={handleClick}>X</div>
+            <h2>{isSignUp ? 'CREATE ACCOUNT' : 'LOG IN'}</h2>
+            <p>By clicking log in, you agree to aour terms ...</p>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="email" 
+                    id="email" 
+                    name="email" 
+                    placeholder="email" 
+                    required={true}  
+                    onChgange={(e) => setEmail(e.target.value)}
+                />
+                <input 
+                    type="password" 
+                    id="password" 
+                    name="password" 
+                    placeholder="password" 
+                    required={true}  
+                    onChgange={(e) => setPassword(e.target.value)}
+                />
+                <input 
+                    type="password-check" 
+                    id="password-check" 
+                    name="password-check" 
+                    placeholder="confirm password" 
+                    required={true}  
+                    onChgange={(e) => setConfirmPassword(e.target.value)}
+                />
+            </form>
+            Auth modal
         </div>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,17 @@
 import logo from '../images/Tinder-Emblema2.png'
 import logo2 from '../images/tinder_logo_white.png'
-const Nav = ( { minimal, authToken }) => {
+const Nav = ( { minimal, authToken, setShowModal, showModal }) => {
+    const handleClick = () => {
+        setShowModal(true)
+    }
+    
     return (
         <nav>
             <div className= "logo-container">
                 <img alt="logo" className="logo" src={minimal ? logo : logo2}/>
             </div>
 
-            {!authToken && !minimal && <button className="nav-button">Log in</button>}
+            {!authToken && !minimal && <button className="nav-button" onClick={handleClick} disabled={showModal}>Log in</button>}
         </nav>
     )
 }
