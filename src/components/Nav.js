17e727cba69e2234404bc38f1e7d16b18d07a215
Felippe Vianna <felippe.conduru@gmail.com>
2022-03-21 19:09:33 -0300

    Auth modal and onboarding pages done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,12 @@
 import logo from '../images/Tinder-Emblema2.png'
 import logo2 from '../images/tinder_logo_white.png'
-const Nav = ( { minimal, authToken, setShowModal, showModal }) => {
+const Nav = ( { minimal, setShowModal, showModal, setIsSignUp }) => {
     const handleClick = () => {
         setShowModal(true)
+        setIsSignUp(false)
     }
+
+    const authToken = true
     
     return (
         <nav>
